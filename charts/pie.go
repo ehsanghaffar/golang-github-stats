11bc0e29ca749b4ad42a6112664e2c7f15d062ec
ehsanghaffarii <ghafari.5000@gmail.com>
2023-02-03 21:21:19 +0330

    improve and add new charts

--- charts/pie.go
+++ charts/pie.go
@@ -1,26 +1,70 @@
 package charts
 
 import (
+	"fmt"
 	"io"
 	"math/rand"
+
+	// "math/rand"
 	"os"
 
+	"github.com/ehsanghaffar/github-stats-golang/api"
 	"github.com/ehsanghaffar/github-stats-golang/utils"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/components"
 	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
+type void struct{}
+
+// var count int
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 // Generate fake data for pie chart
 func genPieItems() []opts.PieData {
-	subjects := []string{"Ehsan", "Ali", "Parsa", "Sara", "Naser", "David"}
+	// subjects := []string{"Ehsan", "Ali", "Parsa", "Sara", "Naser", "David"}
+	// userData := api.GetUserData("ehsanghaffar")
+	v := api.UserRepos("ehsanghaffar")
+	var languages []string
+	// languages := make(map[string]int)
+
+	for _, c := range v {
+		languages = append(languages, c.Language)
+	}
+
+	removeDuplicateStr(languages)
+
+	fmt.Println(languages)
+
 	items := make([]opts.PieData, 0)
-	for i := 0; i < 6; i++ {
+	for i := 0; i < itemCount; i++ {
 		items = append(items, opts.PieData{
-			Name:  subjects[i],
+			Name:  languages[i],
 			Value: rand.Intn(400)})
 	}
+
 	return items
+}
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	fmt.Println("list", list)
+	return list
 }
 
 func pieChart() *charts.Pie {
